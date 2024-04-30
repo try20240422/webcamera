348140cb700993688c6a2b17117b75468cb1a1b4
try20240422 <167757295+try20240422@users.noreply.github.com>
2024-05-01 03:58:22 +0900

    Update webcamera_stream.py

--- webcamera_stream.py
+++ webcamera_stream.py
@@ -23,4 +23,7 @@ angle = st.slider('カメラの角度', 0, 180, 90)
 if st.button('角度を設定'):
     set_servo(angle)
 
+if st.button('ページを更新'):
+    st.experimental_rerun()
+
 st.image(get_camera_image(), caption="Live Camera Feed")
